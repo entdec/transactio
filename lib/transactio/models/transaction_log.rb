aecb7c983651e524803207a88f0095624dda8184
Mark Cornelissen <mark@boxture.com>
2021-04-21 11:21:38 +0200

    Remove comments

--- lib/transactio/models/transaction_log.rb
+++ lib/transactio/models/transaction_log.rb
@@ -6,12 +6,7 @@ module Transactio
 
     has_many :entries, class_name: 'Transactio::TransactionLogEntry'
 
-    # belongs_to :user, optional: true
-    # belongs_to :location, optional: true
-    # belongs_to :account, optional: true
-
     after_commit :clear_current!
-    # before_create :register_user
 
     default_scope -> { order(created_at: :desc) }
 
@@ -24,12 +19,6 @@ module Transactio
     end
 
     private
-
-    # def register_user
-    #   self.user = Current.user
-    #   self.account = Current.account
-    #   self.location = Current.location
-    # end
 
     def clear_current!
       Current.transaction_log = nil
