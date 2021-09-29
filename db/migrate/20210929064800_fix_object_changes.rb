6f387af4d69f56a259c993a8c282ffb9c1422191
Mark Cornelissen <mark@boxture.com>
2021-09-29 06:56:21 +0200

    Add migration to make sure object_changes are JSON objects

---
+++ db/migrate/20210929064800_fix_object_changes.rb
@@ -0,0 +1,11 @@
+class FixObjectChanges < ActiveRecord::Migration[6.0]
+  def up
+    Transactio::TransactionLogEntry.where('jsonb_typeof(object_changes) = ?', 'array').each do |entry|
+      entry.object_changes = entry.object_changes.to_h
+      entry.save!
+    end
+  end
+
+  def down
+  end
+end
--- lib/transactio/frameworks/rails.rb
+++ lib/transactio/frameworks/rails.rb
@@ -1,6 +1,13 @@
 module Transactio
   module Rails
     class Engine < ::Rails::Engine
+      initializer :append_migrations do |app|
+        unless app.root.to_s.match root.to_s
+          config.paths['db/migrate'].expanded.each do |expanded_path|
+            app.config.paths['db/migrate'] << expanded_path
+          end
+        end
+      end
     end
   end
 end
