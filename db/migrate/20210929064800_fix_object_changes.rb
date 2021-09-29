dc01939c678f93bf66c5b504099bacf9de453faf
Mark Cornelissen <mark@boxture.com>
2021-09-29 08:03:33 +0200

    Transaction trickery

--- db/migrate/20210929064800_fix_object_changes.rb
+++ db/migrate/20210929064800_fix_object_changes.rb
@@ -1,8 +1,10 @@
 class FixObjectChanges < ActiveRecord::Migration[6.0]
   def up
     Transactio::TransactionLogEntry.where('jsonb_typeof(object_changes) = ?', 'array').find_each do |entry|
-      entry.object_changes = entry.object_changes.to_h
-      entry.save!
+      Transactio::TransactionLogEntry.transaction do
+        entry.object_changes = entry.object_changes.to_h
+        entry.save!
+      end
     end
   end
 
