98c05b75283d5fd1326db90fd853a0aee1fc33d0
Mark Cornelissen <mark@boxture.com>
2021-09-29 08:37:47 +0200

    Improve migration

--- db/migrate/20210929064800_fix_object_changes.rb
+++ db/migrate/20210929064800_fix_object_changes.rb
@@ -2,9 +2,11 @@ class FixObjectChanges < ActiveRecord::Migration[6.0]
   disable_ddl_transaction!
 
   def up
-    Transactio::TransactionLogEntry.where('jsonb_typeof(object_changes) = ?', 'array').find_each do |entry|
-      entry.object_changes = entry.object_changes.to_h
-      entry.save!
+    while Transactio::TransactionLogEntry.where('jsonb_typeof(object_changes) = ?', 'array').exists? do
+      Transactio::TransactionLogEntry.where('jsonb_typeof(object_changes) = ?', 'array').limit(1000).find_each do |entry|
+        entry.object_changes = entry.object_changes.to_h
+        entry.save!
+      end
     end
   end
 
