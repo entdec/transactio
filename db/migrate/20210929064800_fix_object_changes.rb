201e9fc32064a661d8a7dd8a1d4e8f200c3a8d35
Mark Cornelissen <mark@boxture.com>
2021-09-29 07:46:50 +0200

    And now better

--- db/migrate/20210929064800_fix_object_changes.rb
+++ db/migrate/20210929064800_fix_object_changes.rb
@@ -1,6 +1,6 @@
 class FixObjectChanges < ActiveRecord::Migration[6.0]
   def up
-    Transactio::TransactionLogEntry.where('jsonb_typeof(object_changes) = ?', 'array').each do |entry|
+    Transactio::TransactionLogEntry.where('jsonb_typeof(object_changes) = ?', 'array').find_each do |entry|
       entry.object_changes = entry.object_changes.to_h
       entry.save!
     end
