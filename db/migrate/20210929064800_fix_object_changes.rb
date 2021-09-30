4f2168ac1397ed5bbdf1b17a701a0c18cfedc99d
Mark Cornelissen <mark@boxture.com>
2021-09-30 07:16:58 +0200

    Go per 50k instead of 1k

--- db/migrate/20210929064800_fix_object_changes.rb
+++ db/migrate/20210929064800_fix_object_changes.rb
@@ -3,7 +3,7 @@ class FixObjectChanges < ActiveRecord::Migration[6.0]
 
   def up
     while Transactio::TransactionLogEntry.where('jsonb_typeof(object_changes) = ?', 'array').exists? do
-      Transactio::TransactionLogEntry.where('jsonb_typeof(object_changes) = ?', 'array').limit(1000).find_each do |entry|
+      Transactio::TransactionLogEntry.where('jsonb_typeof(object_changes) = ?', 'array').limit(50000).find_each do |entry|
         entry.object_changes = entry.object_changes.to_h
         entry.save!
       end
