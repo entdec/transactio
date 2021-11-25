cfb1085be8d1589239bffebf37ba1c736321f070
Mark Cornelissen <mark@boxture.com>
2021-11-25 15:30:04 +0100

    Adds convenience methods to Transactio::TransactionLogEntry

--- lib/transactio/models/transaction_log_entry.rb
+++ lib/transactio/models/transaction_log_entry.rb
@@ -8,5 +8,31 @@ module Transactio
     belongs_to :transaction_loggable, polymorphic: true
 
     default_scope -> { order(created_at: :desc) }
+
+    scope :ascending, -> { reorder(created_at: :asc) }
+
+    def object_change?(attribute)
+      return false if object_change_from(attribute) == object_change_to(attribute)
+
+      object_changes.key?(attribute.to_s)
+    end
+
+    def object_change(attribute)
+      return nil if object_change_from(attribute) == object_change_to(attribute)
+
+      object_changes[attribute.to_s]
+    end
+
+    def object_change_from(attribute)
+      return nil if object_changes.nil?
+
+      object_changes[attribute.to_s]&.first
+    end
+
+    def object_change_to(attribute)
+      return nil if object_changes.nil?
+
+      object_changes[attribute.to_s]&.last
+    end
   end
 end
