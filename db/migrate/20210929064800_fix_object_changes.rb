a83941423a619b35e395421577cfc8ac8d6ddd4e
Tom de Grunt <tom@degrunt.nl>
2022-09-30 13:39:18 +0200

    Only when table exists

--- db/migrate/20210929064800_fix_object_changes.rb
+++ db/migrate/20210929064800_fix_object_changes.rb
@@ -2,6 +2,8 @@ class FixObjectChanges < ActiveRecord::Migration[6.0]
   disable_ddl_transaction!
 
   def up
+    return unless ActiveRecord::Base.connection.data_source_exists? 'transaction_log_entries'
+
     while Transactio::TransactionLogEntry.where('jsonb_typeof(object_changes) = ?', 'array').exists? do
       Transactio::TransactionLogEntry.where('jsonb_typeof(object_changes) = ?', 'array').limit(50000).find_each do |entry|
         entry.object_changes = entry.object_changes.to_h
